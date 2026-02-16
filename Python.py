8e1cf85a4a1cd3da8197e167144359c189db2806
irawan-cmd <dilladia08@gmail.com>
2026-02-16 18:11:56 +0700

    Implement ArrayADT class with basic operations

--- Python.py
+++ Python.py
@@ -1,1 +1,23 @@
-print("awokawok")
+class ArrayADT:
+    def __init__(self, size):
+        self.size = size
+        self.data = [0] * size   # inisialisasi isi 0
+
+    # operasi set (ubah nilai)
+    def set(self, index, value):
+        if 0 <= index < self.size:
+            self.data[index] = value
+        else:
+            print("Index di luar batas!")
+
+    # operasi get (ambil nilai)
+    def get(self, index):
+        if 0 <= index < self.size:
+            return self.data[index]
+        else:
+            return "Index di luar batas!"
+
+    # operasi tampilkan isi array
+    def display(self):
+        print(self.data)
+      
